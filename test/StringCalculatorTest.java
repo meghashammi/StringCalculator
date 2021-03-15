9163972db873258f80c8102bf1ca68fa560b7dfd
meghashammi <meghashreeg564@gmail.com>
2021-03-15 20:42:02 +0530

    Adding test cases with comma and new line inputs

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -30,4 +30,16 @@ public class StringCalculatorTest {
         assertEquals(calculator.add("1"), 1);
         assertEquals(calculator.add("5"), 5);
     }  
+    
+    @Test
+    public void additionOfNumbersWithComma() {
+        assertEquals(calculator.add("1,2"), 3);
+        assertEquals(25, calculator.add("10,15"));
+    }
+    
+    @Test
+    public void additionOfNumbersWithNewLine() {
+        assertEquals(calculator.add("1\n2"), 3);
+        assertEquals(calculator.add("11\n13"), 24);
+    }
 }
